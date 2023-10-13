a0a359144725f0219c2301c9856a3a413a13f100
totallynotbirds <123014759+totallynotbirds@users.noreply.github.com>
2023-10-13 10:54:59 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,12 +1,8 @@
 var score = 1; // Create a variable to store the score in
 
-var title = new Title('Welcome to the Game'); // Create a title element
-var button = new Button('hello', btnPress);   // Create a button element
-var hidden = new Button('Boo!');
+var title = new Title('eternity');
 //hidden.hide();
-var scr = new Text(score);                    // Create a text element
-//changes
-setInterval(btnPress, 1000);   // Set up a loop
+var scr = new Text(score);    
 
 function btnPress() {
   score++;
