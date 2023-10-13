9973865c2d458cf04eed986c35ce7848d799b551
totallynotbirds <123014759+totallynotbirds@users.noreply.github.com>
2023-10-13 11:03:42 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 var score = 0; 
 var title = new Title('eternity');
-var button = new Button('gather time' btnpress);
+createButton('gather time' btnpress);
 var scr = new Text(score);    
 function btnPress('gather time') {
   score++;
